4804245cd69eec1825dedcdaeef67478426ccdbd
Telpenarmo <telpenarmo@protonmail.com>
2023-12-22 16:26:40 +0100

    Add required parser files

---
+++ parser/src/language.rs
@@ -0,0 +1,24 @@
+use rowan::Language;
+
+use crate::generated::syntax_kinds::SyntaxKind;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum RideMLLanguage {}
+impl Language for RideMLLanguage {
+	type Kind = SyntaxKind;
+
+	fn kind_from_raw(raw: rowan::SyntaxKind) -> SyntaxKind {
+		SyntaxKind::from_raw(raw.0)
+	}
+
+	fn kind_to_raw(kind: SyntaxKind) -> rowan::SyntaxKind {
+		rowan::SyntaxKind(kind.into_raw())
+	}
+}
+
+pub type SyntaxNode = rowan::SyntaxNode<RideMLLanguage>;
+pub type SyntaxToken = rowan::SyntaxToken<RideMLLanguage>;
+pub type SyntaxElement = rowan::SyntaxElement<RideMLLanguage>;
+pub type SyntaxNodeChildren = rowan::SyntaxNodeChildren<RideMLLanguage>;
+pub type SyntaxElementChildren = rowan::SyntaxElementChildren<RideMLLanguage>;
+pub type PreorderWithTokens = rowan::api::PreorderWithTokens<RideMLLanguage>;
--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -1,14 +1,8 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+mod ast;
+mod generated;
+mod language;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use ast::{AstChildren, AstNode, AstToken};
+pub use generated::{nodes, syntax_kinds::SyntaxKind};
+pub use language::*;
+// pub use token_set::SyntaxKindSet;
