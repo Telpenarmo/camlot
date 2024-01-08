efb7f22635630f1037c8790a59fa68da444693fc
Telpenarmo <telpenarmo@protonmail.com>
2024-01-08 22:55:58 +0100

    Parse stdin and pretty-print CST to stdout

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -28,3 +28,24 @@ pub fn parse(input: &str) -> Parse {
     let sink = sink::Sink::new(&tokens, events);
     sink.finish()
 }
+
+impl Parse {
+    pub fn debug_tree(&self) -> String {
+        let mut s = String::new();
+
+        let tree = format!("{:#?}", self.syntax());
+
+        // We cut off the last byte because formatting the SyntaxNode adds on a newline at the end.
+        s.push_str(&tree[0..tree.len() - 1]);
+
+        for error in &self.errors {
+            s.push_str(&format!("\n{}", error));
+        }
+
+        s
+    }
+
+    pub fn syntax(&self) -> SyntaxNode {
+        SyntaxNode::new_root(self.green_node.clone())
+    }
+}
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,12 @@
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    parse_input();
+}
+
+fn parse_input() {
+    let mut input = String::new();
+    std::io::stdin().read_to_string(&mut input).expect("stdin");
+    let parse = parser::parse(&input);
+    println!("{}\n", parse.debug_tree());
 }
