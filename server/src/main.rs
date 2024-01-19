9d01a14dc4c947643482f3e2fac36accf58755db
Telpenarmo <telpenarmo@protonmail.com>
2024-01-19 16:46:44 +0100

    Introduce (not implemented) REPL and LSP mode

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,41 @@
 use std::io::Read;
 
 fn main() {
-    parse_input();
+    let mode = parse_args();
+    eprintln!("mode: {:?}", mode);
+
+    match mode {
+        Mode::Parse => parse_input(),
+        Mode::REPL => {
+            eprintln!("Repl mode is not yet implemented.");
+            std::process::exit(1);
+        }
+        Mode::LSP => {
+            eprintln!("Lsp mode is not yet implemented.");
+            std::process::exit(1);
+        }
+    }
+}
+
+#[derive(Debug)]
+enum Mode {
+    Parse,
+    REPL,
+    LSP,
+}
+
+fn parse_args() -> Mode {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        Mode::Parse
+    } else {
+        match args[1].as_str().to_lowercase().as_str() {
+            "repl" => Mode::REPL,
+            "parse" => Mode::Parse,
+            "lsp" => Mode::LSP,
+            _ => Mode::REPL,
+        }
+    }
 }
 
 fn parse_input() {
