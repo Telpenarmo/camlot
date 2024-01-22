d0ab5bad61434efeb79bb02d3293eeb9ed2a4b51
Telpenarmo <telpenarmo@protonmail.com>
2024-01-22 23:02:46 +0100

    Change the extension to support rideml
    It assumes the server is available in $PATH as 'rideml-server'.
    As strange as it sounds, this makes vscode extension quite complete,
    in a bare-bones, although totally usable form.
    
    All the rest is to be done in the language server itself.

--- editors/vscode/src/extension.ts
+++ editors/vscode/src/extension.ts
@@ -2,25 +2,46 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+import {
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+} from "vscode-languageclient/node";
+
+let client: LanguageClient | undefined;
+
+async function initLanguageClient() {
+	const serverOptions: ServerOptions = {
+		command: "rideml-server",
+		args: ["lsp"],
+	};
+	const clientOptions: LanguageClientOptions = {
+		documentSelector: [{ scheme: "file", language: "rideml" }],
+		synchronize: {
+			fileEvents: vscode.workspace.createFileSystemWatcher("**/*.{rml,rmli}"),
+		},
+	};
+
+	return new LanguageClient("rideml-lsp", "Rideml Language Server", serverOptions, clientOptions);
+}
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "rideml-analyzer" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('rideml-analyzer.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from rideml-analyzer!');
-	});
-
-	context.subscriptions.push(disposable);
+	if (!client) {
+		try {
+			client = await initLanguageClient();
+			await client.start();
+		} catch (err: any) {
+			vscode.window.showErrorMessage(err);
+		}
+	}
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+	if (client) {
+		client.stop();
+	}
+}
