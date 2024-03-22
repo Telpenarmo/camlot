1d591d703a3e56db828e9d7efb26c2c4e9da04a6
Telpenarmo <telpenarmo@protonmail.com>
2024-03-22 22:14:22 +0100

    Remove scaffold's comment

--- server/src/lsp.rs
+++ server/src/lsp.rs
@@ -1,46 +1,3 @@
-//! A minimal example LSP server that can only respond to the `gotoDefinition` request. To use
-//! this example, execute it and then send an `initialize` request.
-//!
-//! ```no_run
-//! Content-Length: 85
-//!
-//! {"jsonrpc": "2.0", "method": "initialize", "id": 1, "params": {"capabilities": {}}}
-//! ```
-//!
-//! This will respond with a server response. Then send it a `initialized` notification which will
-//! have no response.
-//!
-//! ```no_run
-//! Content-Length: 59
-//!
-//! {"jsonrpc": "2.0", "method": "initialized", "params": {}}
-//! ```
-//!
-//! Once these two are sent, then we enter the main loop of the server. The only request this
-//! example can handle is `gotoDefinition`:
-//!
-//! ```no_run
-//! Content-Length: 159
-//!
-//! {"jsonrpc": "2.0", "method": "textDocument/definition", "id": 2, "params": {"textDocument": {"uri": "file://temp"}, "position": {"line": 1, "character": 1}}}
-//! ```
-//!
-//! To finish up without errors, send a shutdown request:
-//!
-//! ```no_run
-//! Content-Length: 67
-//!
-//! {"jsonrpc": "2.0", "method": "shutdown", "id": 3, "params": null}
-//! ```
-//!
-//! The server will exit the main loop and finally we send a `shutdown` notification to stop
-//! the server.
-//!
-//! ```
-//! Content-Length: 54
-//!
-//! {"jsonrpc": "2.0", "method": "exit", "params": null}
-//! ```
 use std::collections::HashMap;
 use std::error::Error;
 
@@ -54,11 +11,8 @@ use lsp_types::{InitializeParams, ServerCapabilities};
 use lsp_server::{Connection, ExtractError, Message, Request, RequestId, Response, ResponseError};
 
 pub(crate) fn main() -> Result<(), Box<dyn Error + Sync + Send>> {
-    // Note that  we must have our logging only write out to stderr.
     eprintln!("starting generic LSP server");
 
-    // Create the transport. Includes the stdio (stdin and stdout) versions but this could
-    // also be implemented to use sockets or HTTP.
     let (connection, io_threads) = Connection::stdio();
 
     let mut lsp = Lsp::new();
@@ -83,7 +37,6 @@ pub(crate) fn main() -> Result<(), Box<dyn Error + Sync + Send>> {
         // })
     }));
 
-    // Run the server and wait for the two threads to end (typically by trigger LSP Exit event).
     let server_capabilities = serde_json::to_value(&ServerCapabilities {
         text_document_sync: Some(lsp_types::TextDocumentSyncCapability::Kind(
             lsp_types::TextDocumentSyncKind::FULL,
@@ -103,7 +56,6 @@ pub(crate) fn main() -> Result<(), Box<dyn Error + Sync + Send>> {
     main_loop(lsp, connection, initialization_params)?;
     io_threads.join()?;
 
-    // Shut down gracefully.
     eprintln!("shutting down server");
     Ok(())
 }
